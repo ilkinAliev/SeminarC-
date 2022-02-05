695b5108dd21680e6c42a0adf730e0e5dbb74757
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-05 16:48:55 +0300

    Task 07

---
+++ Seminar1/task_07/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать числа от -N до N
+
+Console.Write("введите число N - ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N > 0)
+{
+    int a = - N;
+    while (a <= N)
+    {
+        Console.Write(a + " ");
+        a++;
+    }
+}
+else
+{
+    int a = N;
+    while (a <= -N)
+    {
+        Console.Write(a + " ");
+        a++;
+    }  
+}

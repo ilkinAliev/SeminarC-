5505e4dafb02343dbdf5a231f807a97b4f545359
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-05 15:41:28 +0300

    Task 08

---
+++ Seminar1/task_08/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("введите число - ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int k = 2;
+while (k < n)
+{
+    Console.Write(k + "  ");
+    k = k + 2;
+}

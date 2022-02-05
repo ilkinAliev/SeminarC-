198eed206a4ff54ef9133d2ad017e53ff15b583c
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-05 10:10:47 +0300

    TASK_0

---
+++ Seminar1/task_0/Program.cs
@@ -0,0 +1,6 @@
+﻿// вывести квадрат числа
+Console.Write("введите число ");
+int n = int.Parse(Console.ReadLine());
+//int n = new Random().Next(1, 10);
+Console.WriteLine("n=" + n);
+Console.WriteLine("n^2=" + n*n);

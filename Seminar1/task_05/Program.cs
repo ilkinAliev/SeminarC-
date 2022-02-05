bee53551d03d5c6b60af113a72c7851d0b53f0aa
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-05 16:32:43 +0300

    Task 05

---
+++ Seminar1/task_05/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+// Допустим f(a)= a -2 *3
+Console.Write("введите значение переменной а - ");
+int a = Convert.ToInt32(Console.ReadLine());
+int y = 0;
+
+y = a - 2 * 3;
+
+Console.WriteLine("y = " + y);
+

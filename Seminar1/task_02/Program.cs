7bba80236eb8bc65d2c6329973a13f7ae54ae275
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-05 13:34:54 +0300

    fixes

---
+++ Seminar1/task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int n = new Random().Next(1, 1001);
+Console.Write(" первое число = " + n);
+
+int p = new Random().Next(1, 1001);
+Console.WriteLine(" второе число = " + p);
+
+if( n > p)
+{
+    Console.WriteLine(n + " > " + p);
+}
+else if(n < p) 
+{
+    Console.WriteLine(n + " < " + p);
+}
+else
+{
+    Console.WriteLine( n + " = " + p);
+}

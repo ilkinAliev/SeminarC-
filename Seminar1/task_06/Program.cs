a9f90935ca19356e0f0d21a7f7fd60d9715aa44e
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-05 15:04:34 +0300

    TASK 06

---
+++ Seminar1/task_06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if( n % 2 == 0)
+{
+    Console.WriteLine("число " + n + " - четное");
+}
+else
+{
+    Console.WriteLine("число " + n + " - нечетное");  
+}

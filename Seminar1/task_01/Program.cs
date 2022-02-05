95693794c1b74529ee270a8bddc6cbebb9de0296
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-05 13:04:25 +0300

    some fixes

---
+++ Seminar1/task_01/Program.cs
@@ -0,0 +1,27 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.Write("введите первое число n = ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("введите второе число p = ");
+int p = int.Parse(Console.ReadLine());
+
+if(n == p*p)
+{
+    Console.Write("число " + n);
+    Console.WriteLine(" квадрат числа " + p);
+    Console.WriteLine(n + " = " +  p + " * " +  p  );
+  
+}
+else if(p == n*n)
+{
+        Console.Write("число " + p);
+    Console.WriteLine(" квадрат числа " + n);
+    Console.WriteLine(p + " = " +  n + " * " +  n  );
+}
+else
+{
+    Console.Write( n + " и " + p + " не являются квадратами друг друга");
+   
+}
+
+
---
+++ Seminar1/task_03/Program.cs
@@ -0,0 +1,14 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("введите день недели - ");
+int n = int.Parse(Console.ReadLine());
+
+string[] DayOfWeek = new string[7] { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресение" };
+if (n < DayOfWeek.Length + 1)
+{
+    Console.WriteLine(DayOfWeek[n - 1]);
+}
+else
+{
+    Console.WriteLine("неправильно введен день недели");
+}

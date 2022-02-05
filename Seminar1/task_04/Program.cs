93517804ef0111f8ed757994b09c229be0567edf
ilkin.Aliev <stc_ilkin@mail.ru>
2022-02-05 14:23:33 +0300

    Task 04

---
+++ Seminar1/task_04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти максимальное из трех чисел
+
+int a = new Random().Next(1, 1001);
+Console.Write("число a = " + a);
+
+int b = new Random().Next(1, 1001);
+Console.Write(", число b = " + b);
+
+int c = new Random().Next(1, 1001);
+Console.WriteLine(", число c = " + c);
+
+
+if (a >= b && a >= c)
+{
+    Console.WriteLine("максимальное число = " + a);
+}
+else if (b >= a && b >= c)
+{
+    Console.WriteLine("максимальное число = " + b);
+}
+else
+{
+    Console.WriteLine("максимальное число = " + c);
+}
